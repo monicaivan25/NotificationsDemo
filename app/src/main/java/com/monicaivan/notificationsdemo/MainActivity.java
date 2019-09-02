830667d34d0f3a040e06b483b4fd657ddeeb0950
Monica Ivan <monica.ivan@expleogroup.com>
2019-09-02 16:30:34 +0300

    Finished basic and intent notification

--- app/src/main/java/com/monicaivan/notificationsdemo/MainActivity.java
+++ app/src/main/java/com/monicaivan/notificationsdemo/MainActivity.java
@@ -1,14 +1,79 @@
 package com.monicaivan.notificationsdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private String CHANNEL_ID = "channel_id";
+    private NotificationManagerCompat notificationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+        notificationManager = NotificationManagerCompat.from(this);
+
+        findViewById(R.id.basic_notification_button).setOnClickListener(view -> createBasicNotification());
+        findViewById(R.id.long_text_notification_button).setOnClickListener(view -> createBasicNotificationLongText());
+        findViewById(R.id.intent_notification_button).setOnClickListener(view -> createIntentNotification());
+    }
+
+    private void createBasicNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle("Basic Notification")
+                .setContentText("One line text.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(1, builder.build());
+    }
+
+    private void createBasicNotificationLongText() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle("Notification Title")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("This is a notification with a longer paragraph. This is a notification with a longer paragraph."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(2, builder.build());
+    }
+
+    private void createIntentNotification() {
+        Intent intent = new Intent(this, Cat.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle("Cat")
+                .setContentText("Meow.")
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager.notify(3, builder.build());
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
